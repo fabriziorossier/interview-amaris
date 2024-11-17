f8e456d2516e3078e3d09dbb8e5bf66a2708d43a
Fabrizio Rossier <fabriziorossier@gmail.com>
2024-11-17 19:31:33 -0300

    deleted Pokemon.Pokemon field

---
+++ internal/domain/pokemon.go
@@ -0,0 +1,9 @@
+package domain
+
+type Pokemon struct {
+	Id             int    `json:"id"`
+	Name           string `json:"name"`
+	Height         int    `json:"height"`
+	Weight         int    `json:"weight"`
+	BaseExperience int    `json:"base_experience"`
+}
